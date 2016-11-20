8a2595099a46592e37be61d0e304a6962a2d7fc5
eduardo-salazar <esalazar922@gmail.com>
2016-11-20 18:57:43 +0800

    Fixed clients index

--- spec/web/features/list_clients_spec.rb
+++ spec/web/features/list_clients_spec.rb
@@ -2,10 +2,10 @@ require 'features_helper'
 
 describe 'List clients' do
   it 'displays list of clients on the page' do
-    visit '/books'
+    visit '/clients'
 
     within '#clients' do
-      asset page.has_css?('.client', count: 2), 'Expected to find 2 clients'
+      assert page.has_css?('.client', count: 2), 'Expected to find 2 clients'
     end
   end
 end
