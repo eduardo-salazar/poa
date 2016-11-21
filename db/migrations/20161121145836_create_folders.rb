fc62f45a638af0a0633d97dd41c621a2f1ea6731
eduardo-salazar <esalazar922@gmail.com>
2016-11-21 23:04:15 +0800

    Added migration for table folders related to clients

---
+++ db/migrations/20161121145836_create_folders.rb
@@ -0,0 +1,13 @@
+Hanami::Model.migration do
+  change do
+    create_table :folders do
+      primary_key :id
+
+      column :name,  String, null: false
+      column :description, String, null: false
+      foreign_key :client_id, :clients, on_delete: :cascade, null: false
+      column :created_at, DateTime, null: false
+      column :updated_at, DateTime, null: false
+    end
+  end
+end
