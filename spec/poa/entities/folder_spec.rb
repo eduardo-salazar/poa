af3fe18181943daaab87ae2e9c506b3c9c7d9e59
eduardo-salazar <esalazar922@gmail.com>
2016-11-24 22:31:26 +0800

    Added test for folder initalization attributes

--- spec/poa/entities/folder_spec.rb
+++ spec/poa/entities/folder_spec.rb
@@ -2,4 +2,10 @@ require 'spec_helper'
 
 describe Folder do
   # place your tests here
+  it 'can be initialised with attributes required' do
+    client = Client.new(name: 'Client1', desc: 'Description', location: 'Managua')
+    #create folders
+    folder = folder.new(name: 'Economia', desc: 'Curso de economia para parte sur')
+    folder.name.must_equal 'Economia'
+  end
 end
